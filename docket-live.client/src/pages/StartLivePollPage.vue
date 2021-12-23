179cd0fc8c69715e5f10e66751c43b842308b1e5
justin <JustinCarpenter2020@Gmail.com>
2021-12-23 16:47:29 -0700

    button needs to go center

--- docket-live.client/src/pages/StartLivePollPage.vue
+++ docket-live.client/src/pages/StartLivePollPage.vue
@@ -3,21 +3,21 @@
     <GameNavbar />
     <div class="row text-center">
       <div class="col-12" v-if="activeSession.poll">
-        <h1 class="text-light">{{activeSession.poll.title}}</h1>
-        <h2 class="text-primary">
+        <h1 class=" title-shadow">{{activeSession.poll.title}}</h1>
+        <h2 class="class-shadow">
           {{activeSession.className}} - Week Here
         </h2>
-        <h2 class="text-warning">Join Code: {{activeSession.sessionCode}}</h2>
+        <h2 class="code-shadow">Join Code: {{activeSession.sessionCode}}</h2>
       </div>
     </div>
-    <div class="row justify-content-center ">
-      <div class="col-4">
+    <div class="row justify-content-center">
+      <div class="col-3">
         <p class="text-warning m-0">Who's All Here?</p>
       </div>
       <div class="col-4">
-            <button class="btn start-poll">Start Poll</button>
+            <button class="btn start-poll ms-5 ">Start Poll</button>
       </div>
-      <div class="col-2 text-end">
+      <div class="col-3 text-end">
         <span class="join-timer p-3 text-center">10</span>
       </div>
       <div class="col-10 player-area">
@@ -60,6 +60,24 @@ export default {
 
 
 <style lang="scss" scoped>
+
+.title-shadow{
+  font-size: 75px;
+  color: #FFFFFF;
+text-shadow: 0px 4px 0px #ABC1CD;
+}
+
+.class-shadow{
+color: #3BA5DC;
+text-shadow: 0px 4px 0px #196895;
+font-size: 40px;
+}
+
+.code-shadow{
+  color: #EAD35D;
+text-shadow: 0px 1px 0px #EAA95D;
+font-size: 40px;
+}
 .player-area{
   background: rgba(243, 242, 242, 0.25);
 border: 1px solid #F5F5F5;
@@ -67,7 +85,7 @@ box-sizing: border-box;
 box-shadow: 0px 5px 0px #D7D7D7;
 backdrop-filter: blur(20px);
 border-radius: 5px;
-height: 60vh;
+height: 55vh;
 }
 
 .start-poll{
@@ -77,6 +95,9 @@ border-radius: 50px;
 position: absolute;
  transform: translateY(5px);
 z-index: 1000;
+width: 250px;
+// top: 50%;
+left: 50%;
 }
 
 .join-timer{
